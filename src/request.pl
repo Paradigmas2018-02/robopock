7c1d2abea1fb25824246d9f552df50c7f3f70205
pablodiegoss <pablodiegoss@hotmail.com>
2018-09-28 15:21:58 -0300

    Create DJSON parser for ranks

--- src/request.pl
+++ src/request.pl
@@ -1,6 +1,12 @@
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
+:- pack_install(djson).
+:- use_module(library(djson)).
+:- multifile djson:json//1.
 
+
+djson:json(rank(X,Y)) -->
+    json({ value: X, suit: Y }).
 
 deck :- http_open('http://localhost:3000/deck', In, []),
             json_read(In, Deck).
